a04be89804d1c5ee7f16450510d1840639b0aca9
이지민 <1zzimy@naver.com>
2025-06-20 21:55:48 +0900

    feat: JPA 설정 및 Entity/Repository 기본 구조 추가

--- src/main/java/com/babit/demo/domain/user/entity/User.java
+++ src/main/java/com/babit/demo/domain/user/entity/User.java
@@ -1,4 +1,11 @@
 package com.babit.demo.domain.user.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
