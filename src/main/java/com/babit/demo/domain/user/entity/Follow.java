9278e7427e61605c52faa656fe8cf0fa69de0013
이지민 <1zzimy@naver.com>
2025-06-26 02:45:30 +0900

    refactor: Follow 엔티티에 Lombok 및 JPA 필수 어노테이션 추가

--- src/main/java/com/babit/demo/domain/user/entity/Follow.java
+++ src/main/java/com/babit/demo/domain/user/entity/Follow.java
@@ -1,11 +1,18 @@
 package com.babit.demo.domain.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "follows",
         uniqueConstraints = {@UniqueConstraint(columnNames = {"follower_id", "following_id"})})
 public class Follow {
@@ -22,5 +29,11 @@ public class Follow {
     private User following;
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    public Follow(User follower, User following) {
+        this.follower = follower;
+        this.following = following;
+    }
 }
