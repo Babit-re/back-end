413d2201de7010cacbf86f9a103b71a9c0d1ffa7
jiyunkim <jiyunkim831@gmail.com>
2025-06-25 23:21:56 +0900

    feat: 식단에 필요한 ENUM type 추가

---
+++ src/main/java/com/babit/demo/domain/diet/type/DietType.java
@@ -0,0 +1,5 @@
+package com.babit.demo.domain.diet.type;
+
+public enum DietType {
+    BREAKFAST, LUNCH, DINNER, SNACK
+}
---
+++ src/main/java/com/babit/demo/domain/diet/type/StatusType.java
@@ -0,0 +1,5 @@
+package com.babit.demo.domain.diet.type;
+
+public enum StatusType {
+    LOW, NORMAL, HIGH
+}
