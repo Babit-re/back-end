44ed5342abb81a83b2787a313a40cb195882940b
leechel <chera1009@naver.com>
2025-06-25 21:53:34 +0900

    chore: 마일리지 도메인 추가 및 디렉토리 생성

---
+++ src/main/java/com/babit/demo/domain/mileage/controller/MileageController.java
@@ -0,0 +1,4 @@
+package com.babit.demo.domain.mileage.controller;
+
+public class MileageController {
+}
---
+++ src/main/java/com/babit/demo/domain/mileage/entity/MileageHistory.java
@@ -0,0 +1,4 @@
+package com.babit.demo.domain.mileage.entity;
+
+public class MileageHistory {
+}
