17cad663a37d520d5945e4515d08b04170e841c6
leechel <chera1009@naver.com>
2025-06-25 22:58:31 +0900

    feat: ëMileageHistory 엔티티 작성

--- src/main/java/com/babit/demo/domain/mileage/entity/MileageHistory.java
+++ src/main/java/com/babit/demo/domain/mileage/entity/MileageHistory.java
@@ -1,4 +1,37 @@
 package com.babit.demo.domain.mileage.entity;
 
+import com.babit.demo.domain.user.entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
 public class MileageHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "amount", nullable = false)
+    private int amount;
+
+    @Column(name = "reason", nullable = false)
+    private String reason;
+
+    @Column(name = "target_date", nullable = false)
+    private LocalDate targetDate;
+
+    @Column(name = "granted_at", nullable = false)
+    private LocalDateTime grantedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.grantedAt = LocalDateTime.now();
+    }
+
 }
