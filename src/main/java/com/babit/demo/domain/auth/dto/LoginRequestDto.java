73c3c9e73e40f70dded6cc5224e302854b3f687b
imnyoung <hkny0627@gmail.com>
2025-06-27 01:47:48 +0900

    feat: LoginRequestDto 중 String 속성에 대한 valid 설정을 notnull->notblank로 수정 & 각 속성에 맞는 검증 어노테이션 추가

--- src/main/java/com/babit/demo/domain/auth/dto/LoginRequestDto.java
+++ src/main/java/com/babit/demo/domain/auth/dto/LoginRequestDto.java
@@ -1,6 +1,8 @@
 package com.babit.demo.domain.auth.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,10 +11,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginRequestDto {
-    @NotNull(message = "이메일을 입력하세요.")
+    @NotBlank(message = "이메일을 입력하세요.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @NotNull(message = "비밀번호를 입력하세요.")
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Size(min = 4, max = 12, message = "비밀번호는 4자 이상 12자 이하여야합니다.")
     private String password;
 
     @Builder
